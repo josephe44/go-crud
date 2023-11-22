109e5f78be8a4c40c52913204e177872db88d6f9
josephe44 <josephe442@gmail.com>
2023-11-22 16:39:04 +0100

    first go crud server api

--- controllers/userController.go
+++ controllers/userController.go
@@ -117,6 +117,10 @@ func Login(c *gin.Context) {
 	c.SetSameSite(http.SameSiteLaxMode)
 	c.SetCookie("Authorization", tokenString, 3600*24*30, "", "", false, true)
 
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Logged in successfully",
+	})
+
 	// send it back - send the token as response
 	// c.JSON(http.StatusOK, gin.H{
 	// 	"token": tokenString,
